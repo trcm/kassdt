9bbb5fff308405eec833cdb43ff2692cd009f70a
Kieran Eames <Xagefu@d-i89-161-111.student.eduroam.uq.edu.au>
2014-08-08 13:36:35 +1000

    Made Some clearer changes to views.py.

--- code_review/lti/views.py
+++ code_review/lti/views.py
@@ -3,9 +3,9 @@ from django.http import HttpResponse
 
 def index(request):
     #Add here the requests from ims_lti_py to create /autehnticate users
-    #NEED KEYS AND STUFF 
-    #ToolProvider.__init__
-
+    #So none of this will work until we have permissions
+    tool = ToolProvider('consumer_key', 'consumer_secret', 'params={}')
+    
 
 
     return HttpResponse()
