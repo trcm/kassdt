1f74ed2b7a988e441e2db3efa981d65748643072
Kieran Eames <Xagefu@Kierans-MacBook-Pro-2.local>
2014-08-08 11:36:36 +1000

    Added Lti framework, cant test if working until we have an approved connection, to install the lti-pyhton connector go here, https://github.com/starteam/ims_lti_py Ignore the pip install instrucitons just clone and run the setup.py

--- code_review/code_review/settings.py
+++ code_review/code_review/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'review',
     'south',
+    'ims_lti_py',
 )
 
 MIDDLEWARE_CLASSES = (
---
+++ code_review/lti/views.py
@@ -0,0 +1,11 @@
+from ims_lti_py import *
+from django.http import HttpResponse
+
+def index(request):
+    #Add here the requests from ims_lti_py to create /autehnticate users
+    #NEED KEYS AND STUFF 
+    #ToolProvider.__init__
+
+
+
+    return HttpResponse()
