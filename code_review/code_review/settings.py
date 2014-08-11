e8a83b576b7fc9daf399774087be14f04756f034
Alexia Lee <alexia.lee@uqconnect.edu.au>
2014-08-11 23:26:01 +1000

    Changed MEDIA_ROOT in settings to BASE_DIR joined with 'media'. Need to pull from dev branch and git doesn't let me unless I commit? Need to find some way to force it

--- code_review/code_review/settings.py
+++ code_review/code_review/settings.py
@@ -106,7 +106,7 @@ USE_L10N = True
 USE_TZ = True
 
 
-MEDIA_ROOT = 'storage/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
