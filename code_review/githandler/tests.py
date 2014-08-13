3cafab394477d1503ed4e6fa0619f1135f9491c8
Alexia Lee <alexia.lee@uqconnect.edu.au>
2014-08-13 21:38:40 +1000

    Started writing tests but hmm it won't run because of some other test in the project.

--- code_review/githandler/tests.py
+++ code_review/githandler/tests.py
@@ -6,7 +6,8 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
-
+from review.models import *
+from githandler.handler import *
 
 class SimpleTest(TestCase):
     def test_basic_addition(self):
@@ -14,3 +15,16 @@ class SimpleTest(TestCase):
         Tests that 1 + 1 always equals 2.
         """
         self.assertEqual(1 + 1, 2)
+
+'''
+    Test cloning from public repo
+'''
+
+'''
+    Test that database is populate correctly
+'''
+class PopulateDatabaseTest(TestCase):
+    
+    def setUp(self):
+	print('Getting a Repo object to do tests on.')
+	print('By default we clone to MEDIA_ROOT/media/githandle_test')
