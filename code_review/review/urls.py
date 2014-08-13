80e4cc06e7f65f10be3708c3f8cd779e715042ff
tom <midson.trc@gmail.com>
2014-08-13 13:19:52 +1000

    Added a bit more styling with bootstrap, just to make it look a little better for the prototype removed a few bits of repeated code

--- code_review/review/urls.py
+++ code_review/review/urls.py
@@ -19,6 +19,7 @@ urlpatterns = patterns('',
                            {'next_page': '/review/'}, name='logout'),
                        # Furture route to register new users.  This probably won't be used
                        url(r'register/$', views.index, name='register'),
+                       url(r'course_admin/$', views.index, name='default'),
                        url(r'course_admin/(?P<course_code>[A-Z]{4}[0-9]{4})/$',
                            views.adminRedirect, name='adminRedirect'),
                        url(r'create_assignment/(?P<course_code>[A-Z]{4}[0-9]{4})/$',
