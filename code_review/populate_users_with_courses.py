bbf3956271be4bbffb290104725594586cd97304
Tom <midson.trc@gmail.com>
2014-08-16 00:29:54 +1000

    finished script to populate users and courses from a csv file

--- code_review/populate_users_with_courses.py
+++ code_review/populate_users_with_courses.py
@@ -8,14 +8,33 @@ os.environ['DJANGO_SETTINGS_MODULE'] = 'code_review.settings'
 from review.models import Course, User, ReviewUser
 
 users = []
-
+rUser = None
 if sys.argv.__len__() > 1:
     with open(sys.argv[1], 'r') as csvfile:
         reader = csv.reader(csvfile, delimiter=',')
         for row in reader:
             users.append(row)
 
+print users
+            
 for r in users:
-    print r[0], r[1]
-    for x in range(2, r.__len__()):
-        print r[x]
+    newUser = User.objects.get_or_create(username=r[0])
+    print newUser
+
+    if newUser[1] == True:
+        newUser[0].set_password(r[1])
+        newUser[0].is_staff=False
+        rUser = ReviewUser.objects.create(djangoUser=newUser[0],
+                isStaff=False)
+        newUser[0].save()
+        rUser.save()
+        for x in range(2, r.__len__()):
+            print r[x]
+            toAdd = Course.objects.get(course_code=r[x].upper())
+
+            rUser.courses.add(toAdd)
+            rUser.save()
+
+    else:
+        print newUser[0].username
+    
