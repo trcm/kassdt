a924a81897be2e9e9790004483958e6029a387eb
Tom <midson.trc@gmail.com>
2014-08-18 15:12:11 +1000

    Changed adminRedirect to be courses, it now points the the individual course pages.  Fixed error in user population script.

--- code_review/review/views.py
+++ code_review/review/views.py
@@ -23,7 +23,7 @@ from forms import AssignmentForm, UserCreationForm
 # It will also list all the courses they're currently
 @login_required
 # @user_passes_test(staffTest)
-def adminRedirect(request, course_code):
+def coursePage(request, course_code):
     context = {}
     # get the current assignments for the subject, subject choosing
     # will be added later
@@ -38,12 +38,12 @@ def adminRedirect(request, course_code):
         print "Getting courses"
         courses = U.reviewuser.courses.all()
         context['courses'] = courses
-        return render(request, 'admin.html', context)
+        return render(request, 'course_page.html', context)
     except Exception as UserExcept:
         print UserExcept.args
         raise Http404
 
-    return render(request, 'admin.html', context)
+    return render(request, 'course_page.html', context)
 
 # gets the course code for the current course being used and
 # creates a form for creating a new assignment, redirects to the
@@ -117,7 +117,6 @@ def index(request):
     # generated here
     U = User.objects.get(id=request.user.id)
     context['user'] = U
-
     try:
         courses = U.reviewuser.courses.all()
         context['courses'] = courses
@@ -190,7 +189,8 @@ def validateAssignment(request):
 
 # validates the data for user createion, pretty much the same as the view above
 # but for users.  Also creates a new review user for the user
-    
+
+
 @login_required
 @user_passes_test(staffTest)
 def validateUser(request):
@@ -218,7 +218,7 @@ def validateUser(request):
                 newRUser = ReviewUser.objects.create(djangoUser=newUser,
                                                      isStaff=is_staff)
                 print User.objects.filter(username=username).count()
-                
+
                 try:
                     newUser.save()
                     newRUser.save()
@@ -226,7 +226,7 @@ def validateUser(request):
                     print r.args
                     context['error'] = "Errr Something went wrong, Tom fix this"
                     return render(request, 'admin/userCreate.html', context)
-                    
+
                 context['users'] = User.objects.all()
                 return render(request, 'admin/userList.html', context)
             except Exception as ValidationError:
@@ -234,7 +234,7 @@ def validateUser(request):
 
     context['form'] = form
     return render(request, 'admin/userCreate.html', context)
-    
+
 
 @login_required
 @user_passes_test(staffTest)
@@ -244,13 +244,13 @@ def validateCourse(request):
 
     if request.method == "POST":
         form = CourseCreationForm(request.POST)
-        
+
         if form.is_valid():
             try:
                 course_code = form.cleaned_data['course_code']
                 course_name = form.cleaned_data['course_name']
                 newCourse = Course.objects.reate(course_code=course_code,
-                        course_name=course_name)
+                                                 course_name=course_name)
                 try:
                     newCourse.save()
                 except:
@@ -258,9 +258,11 @@ def validateCourse(request):
 
                 context['courses'] = Course.objects.all()
                 return render(request, 'admin/courseList.html', context)
-                
+
             except Exception as ValidationError:
                 print ValidationError.args
 
     context['form'] = form
-    return render(request, '/admin/courseCreate.html', context) 
+    return render(request, '/admin/courseCreate.html', context)
+
+
