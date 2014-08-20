5ffd1fa1b213654763c513c9df5a1dc6a397ed22
Alexia Lee <alexia.lee@uqconnect.edu.au>
2014-08-20 20:09:53 +1000

    Technically not really a unit test but the current test tests that we can clone and populate db with correct folder structure.

--- code_review/review/tests_git_handler.py
+++ code_review/review/tests_git_handler.py
@@ -24,8 +24,10 @@ class RepoTests(TestCase):
     def test_public_populate_db(self):
         # Get the Repo object
         url = "https://github.com/avadendas/public_test_repo.git"
+        rootFolder = "githandler_test"
+        studentRepo ="test_student_repo"
         # Needed for call to populate_db
-        relDir = os.path.join("githandler_test", strftime("%Y-%m-%d_%H:%M:%S"))
+        relDir = os.path.join(rootFolder, strftime("%Y-%m-%d_%H:%M:%S"))
         directory = os.path.join(settings.MEDIA_ROOT, relDir)
         
         # Create some fake assignments 
@@ -38,3 +40,44 @@ class RepoTests(TestCase):
 
         # Do it! 
         populate_db(sub, relDir)
+
+        # Now check the directory structure is all good.
+        folders = SourceFolder.objects.all()
+        
+        # Check that folders have all been created and have correct names
+        self.assertEqual(len(folders), 4)
+        # NB this root folder (test_student_repo) will have a different name in future versions
+        self.assertEqual(folders[0].name, studentRepo)
+        self.assertEqual(folders[1].name, 'folder1')
+        self.assertEqual(folders[2].name, 'folder1-sub')
+        self.assertEqual(folders[3].name, 'folder2')
+        
+        # Check that subfolders are correct
+        subfolder = folders[2] # this is folder1-sub
+        self.assertEqual(subfolder.parent.name, 'folder1')
+        self.assertEqual(subfolder.parent.parent.name, studentRepo)
+        subfolder = folders[3] # this is folder2
+        self.assertEqual(subfolder.parent.name, studentRepo)
+
+        # Check all the files are correct
+        files = SourceFile.objects.all()
+        self.assertEqual(len(files), 3)
+        file1 = files[0]
+
+        self.assertEqual(file1.name, 'folder1_testfile.txt')
+        dir = os.path.join(relDir, studentRepo)
+        self.assertEqual(repr(file1.file), '<FieldFile: ' + dir + '/folder1>') 
+        self.assertEqual(file1.folder, folders[1])
+        
+        file2 = files[1]
+        self.assertEqual(file2.name, 'folder1-sub-testfile.py')
+        self.assertEqual(file2.folder, folders[2])
+        
+        file3 = files[2]
+        self.assertEqual(file3.name, 'folder2_testfile.py')
+        self.assertEqual(file3.folder, folders[3])
+        
+       
+
+
+
