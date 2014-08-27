1ab8e10cd53ea391f508244dc45948a21fde56cd
Alexia Lee <alexia.lee@uqconnect.edu.au>
2014-08-28 09:19:16 +1000

    Fixed asmt submission view so that if something goes wrong during submission creation process, it deletes the half-formed submission. But. Some peculiar thing with git seems to have happened again, and modifying the view is having no effect... D:

--- code_review/review/views.py
+++ code_review/review/views.py
@@ -381,7 +381,13 @@ def submit_assignment(request, course_code, asmt):
             
             except GitCommandError as giterr:
                 print giterr.args
+                sub.delete()
                 context['errMsg'] = "Something wrong with the repository URL."
+                template = 'assignment_submission.html'
+
+            except Exception as e:
+                sub.delete()
+                context['errMsg'] = "Unknown error occurred, contact sys admin."
                 template = 'assignment_submission.html'
             
             # Populate databse.
