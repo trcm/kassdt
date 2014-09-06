113bc51ecc985fe4ea3149d746700fce0b8e0a35
Alexia Lee <alexia.lee@uqconnect.edu.au>
2014-09-06 19:52:57 +1000

    forms.py -- changed the submission field from URLField to CharField. this is still OK because if they enter gibberish git will pick it up. Verified that we can now take git@github.com:avadendas/private_test_repo.git type of addresses, and will clone successfully as long as the user has added the ssh key to their repo. Also shows appropriate error messages if user enters blank string or gobbledygook or extant but non-repo url.
    views.py -- working on some commenting.

--- code_review/review/views.py
+++ code_review/review/views.py
@@ -360,13 +360,19 @@ def student_homepage(request):
     return render(request, 'student_homepage.html', context)
 
 def get_open_assignments(user):
-    '''
-    Grabs the list of currently open assignments for
-    the current user
-    :user User
+    ''' Grabs the list of currently open assignments for the current user
+    
+    Here open assignment means that a student is able to make a submission
+    for the assignment at the time this method is called. I.e., submissions
+    are open and the due date has not passed yet. 
 
-    return List[(Course, Assignment)]
+    Arguments:
+        user (User) -- the user whose open assignments we want to retrieve.
+    
+    Returns:
+        A list whose entries are a tuple (Course, Assignment) 
     '''
+    
     timenow = timezone.now()
     openAsmts = []
     courses = user.reviewuser.courses.all()
@@ -380,11 +386,18 @@ def get_open_assignments(user):
 
 @login_required(login_url='/review/login_redirect/')
 def assignment_page(request, course_code, asmt):
+    '''Displays the data (due date, review open date etc.) for a specific assignment.
+
+    Arguments:
+        request (HttpRequest) -- the HTTP request asking to view the assignment.
+        course_code (Course.course_code) -- the course code of the course to which
+                                            the assignment we want to display belongs.
+        asmt (Assignment) -- the assignment we want to display. 
+    
+    Returns:
+        A HttpResponse object which is used to render the webpage.
     '''
-    assignment_page Displays the data for a specific assignment
-    :course_code Course.course_code
-    :asmt Assignment
-    '''
+
     context = {}
 
     U = User.objects.get(id=request.user.id)
@@ -403,22 +416,43 @@ def assignment_page(request, course_code, asmt):
     return render(request, 'assignment_page.html', context)
 
 def can_submit(asmt):
-    '''
-    :asmt Assignment
+    '''Check whether a student can make a submission to an assignment. 
 
-    Return True if allowed to submit asmt now
-    False otherwise
+    Checks whether asmt is open for submission; i.e., determine whether or not
+    submissions have opened, and whether the due date has passed. 
+
+    Arguments:
+        asmt (Assignment) -- the assignment for which we want to check whether
+                             or not submission are open. 
+
+    Returns:
+        True if allowed to submit asmt now, False otherwise
     '''
+
     now = timezone.now()
     return now < asmt.submission_close_date and now > asmt.submission_open_date
 
 
 @login_required(login_url='/review/login_redirect/')
 def submit_assignment(request, course_code, asmt):
-    """
-    :request the HTTP request object
-    :course_code String course code
-    :asmt Assignment the assignment for which we want to submit
+    """Make a submission for an assignment.
+
+    When the user enters the address of their reposistory (e.g., on github) 
+    into the submission page, this method is called to clone the contents of the
+    given repository and populate the database with an AssignmentSubmission and 
+    SourceFolder and SourceFile objects. 
+
+    Arguments:
+        request (HttpRequest) -- the HTTP request object asking to make a submission.
+        course_code (String) -- the course code of the course to which
+                                the assignment belongs. 
+        asmt (Assignment) -- the assignment for which the student wants to submit.
+    
+    Returns:
+        A HttpReponse object which renders a confirmation page if the submission
+        is successful (the given repository exists, cloning succeeded), or which
+        re-renders the submission page with appropriate error messages 
+        (e.g., URL entered does not exist) if the submission is unsuccessful. 
 
     TODO - handle multiple submission and single-submission assignments
            differently
