f35103beac4dd8cce9813264fbf5f4beac58b39b
Alexia Lee <alexia.lee@uqconnect.edu.au>
2014-11-01 20:07:39 +1000

    Renamed the functional testing for assignment tests. Added one more "unit" test for git_handler.

--- code_review/review/tests_git_handler.py
+++ code_review/review/tests_git_handler.py
@@ -26,6 +26,7 @@ class RepoTests(TestCase):
         self.asmt = Assignment.objects.create(course_code=self.course, name='TestAsmt', submission_close_date=timezone.now(), review_close_date=timezone.now())
         self.sub = AssignmentSubmission.objects.create(by=self.user, submission_for=self.asmt)
     
+    """
     def test_clone(self):
         '''Not necessary?'''
         pass 
@@ -47,8 +48,77 @@ class RepoTests(TestCase):
     def test_traverse_tree(self):
         '''Test that the database is updated correctly after cloning.
         I.e., check that the folder structures all match.
+        This test is not really necessary because it is tested 
+        implicitly by 
+        test_private_populate_db and test_public_populate_db. 
         '''
         pass 
+    """
+
+    def test_populate_from_local(self):
+        '''Check that database gets populated correctly when assignment
+        submission made by calling populate_from_local.
+        '''
+        url = "https://github.com/avadendas/public_test_repo.git"
+        sub = self.sub
+        sub.submission_repository = url
+        
+        # Get the Repo object
+        rootFolder = "githandler_test"
+        studentRepo = root_folder_name(sub)
+        # Needed for call to populate_db
+        relDir = os.path.join(rootFolder, strftime("%Y-%m-%d_%H:%M:%S"))
+        directory = os.path.join(settings.MEDIA_ROOT, relDir)
+        repoObj = Repo.clone_from(url,directory)
+
+        populate_from_local(directory, studentRepo, sub, relDir)
+
+        # Test directory structure is as expected. 
+        folders = SourceFolder.objects.all()
+        
+        # Check that folders have all been created and have correct names
+        self.assertEqual(len(folders), 4)
+        # Repo name involves the time of folder creation so won't match; 
+        # Truncate so that we only have the username part of it.
+        # ind = studentRepo.find('_')
+        # studentRepo = studentRepo[:ind]
+        # In fact because code executes quickly, times are usu. the same.
+        
+        self.assertEqual(folders[0].name, studentRepo)
+        self.assertEqual(folders[1].name, 'folder1')
+        self.assertEqual(folders[2].name, 'folder1-sub')
+        self.assertEqual(folders[3].name, 'folder2')
+        
+        # Check that subfolders are correct
+        subfolder = folders[2] # this is folder1-sub
+        self.assertEqual(subfolder.parent.name, 'folder1')
+        self.assertEqual(subfolder.parent.parent.name, studentRepo)
+        subfolder = folders[3] # this is folder2
+        self.assertEqual(subfolder.parent.name, studentRepo)
+
+        # Check all the files are correct
+        files = SourceFile.objects.all()
+        self.assertEqual(len(files), 4)
+        
+        file0 = files[0]
+        self.assertEqual(file0.name, 'rootfile.txt')
+        # Make sure file was in root directory
+        self.assertEqual(file0.folder.name, studentRepo)
+
+        file1 = files[1]
+        self.assertEqual(file1.name, 'folder1_testfile.txt')
+        dir = os.path.join(relDir, studentRepo)
+        self.assertEqual(repr(file1.file), '<FieldFile: ' + dir + '/folder1/folder1_testfile.txt>') 
+        self.assertEqual(file1.folder, folders[1])
+        
+        file2 = files[2]
+        self.assertEqual(file2.name, 'folder1-sub-testfile.py')
+        self.assertEqual(file2.folder, folders[2])
+        
+        file3 = files[3]
+        self.assertEqual(file3.name, 'folder2_testfile.py')
+        self.assertEqual(file3.folder, folders[3])
+
 
     def test_private_populate_db(self):
         '''
